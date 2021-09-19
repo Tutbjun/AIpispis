e6fafea38c7a1ea9371df3b07948f31c910afc82
Tutbjun <54132574+Tutbjun@users.noreply.github.com>
2021-09-19 19:11:09 +0200

    progress on mesh demo

--- Reference/giraphicsTests/giraphics.py
+++ Reference/giraphicsTests/giraphics.py
@@ -1,15 +1,46 @@
 from ursina import *
 from anytree import Node
+import os
+from zipfile import ZipFile
+import glob
 
-class multMesh():
+os.chdir(os.path.dirname(__file__))
+
+def recursive_search(folder,file_type, ignore=None):
+    abspath = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(abspath)
+    directory = "./"
+    pathname = directory + "/**/*." + file_type
+    files = glob.glob(pathname, recursive=True)
+    files = [e for e in files if "./" + folder + "/" in e]
+    files = ["/".join(".".join(e.split(".")[1:]).split("/")[1:]) for e in files]
+    if ignore != None:
+        files = [e for e in files if ignore not in e]
+    return files
+
+class subMesh():#burde have en enkelt root mesh, og så evt. nogle undermeshes som er med subMesh klassen
+    rootMesh = 0
     subMeshes = []
-    def __init__(self, submeshes):
-        for m in submeshes:
-            self.subMeshes.append(m)
+    def __init__(self, rootMesh, submeshes):
+        self.subMeshes = submeshes
 
-ogMesh = load_model(name="bug.obj")
+def loadMultiMesh(path):
+    with ZipFile(path, 'r') as zip:
+        zip.printdir()
+        zip.extractall()
+        n = os.path.basename(path)
+        n = n.split(".")[0:-1]
+        subMeshPaths = []
+        rootMeshPath = ""
+        for p in os.listdir(n):
+            if 
+        return multMesh([load_model(name=os.path.join("bugObj","root","bug.obj"))])
 
-ent = Entity(model=ogMesh,texture="grass",color=color.color(0, 0, 0.5))
+ogMesh = loadMultiMesh(os.path.join(os.path.dirname(__file__),"bugObj.zip"))
+
+
+
+ent = Entity(model=ogMesh,color=color.color(0, 0, 0.5))
 
 def update():
     print("lmao")
