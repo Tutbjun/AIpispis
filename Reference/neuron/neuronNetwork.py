3128f37d5db002f4cb11c1e28badeb41498e5b85
Tutbjun <54132574+Tutbjun@users.noreply.github.com>
2021-09-25 18:20:11 +0200

    Class restructuring

--- Reference/neuron/neuronNetwork.py
+++ Reference/neuron/neuronNetwork.py
@@ -3,48 +3,79 @@ from ursina import *
 import random
 import time
 
+from ursina.lights import AmbientLight
 
-class NeuronEnt():
-    neuron = 0
-    ent = 0
-    def __init__(self,neuronArgs):
-        self.neuron = Neuron(*neuronArgs)
-        self.ent = Entity(model="sphere",color=color.red,scale=(0.1,0.1,0.1))
-        self.ent.position = self.neuron.pos
 
-class Neuron():
-    pos = np.zeros(3)
-    ODval = 1
-    connects = []
-    allPoses = []
+class Network():
+    neuronEnts = []
+    poses = []
+    def __init__(self):
+        pass
 
-    def __init__(self,allPoses,selfIndex,connects,OD):
-        self.allPoses = allPoses
-        self.pos = allPoses[selfIndex]
-        self.connects = connects
-        self.ODval = OD
+    def initConnects(self):
+        for i in range(len(self.neuronEnts)):
+            for j in range(len(self.neuronEnts[i].neuron.connects)):
+                self.neuronEnts[i].conLines.append(Entity(model=Mesh(vertices=[self.neuronEnts[i].neuron.pos,self.poses[self.neuronEnts[i].neuron.connects[j]]],mode='line')))
+
+    class NeuronEnt():
+        neuron = 0
+        ent = 0
+        conLines = []
+        def __init__(self,neuronArgs):
+            self.neuron = Network.Neuron(*neuronArgs)
+            self.ent = Entity(model="sphere",color=color.red,scale=(0.1,0.1,0.1))
+            self.ent.position = self.neuron.pos
+            
+
+    class Neuron():
+        pos = np.zeros(3)
+        ODval = 1
+        connects = []
+        #allPoses = []
+        def __init__(self,pos,connects,OD):
+            self.pos = pos
+            self.connects = connects
+            self.ODval = OD
 
 def update():
     pass
     #time.sleep(1)
 
-def startUp():
+def initNetwork():
     poses = []
     for i in range(10):
         pos = []
         for j in range(3):
             pos.append(random.uniform(-1,1))
         poses.append(pos)
-    
-    neus = []
+    connects = []
     for i in range(10):
-        neus.append(NeuronEnt((poses,i,[],1)))
+        con = []
+        for j in range(2):
+            con.append(random.choice([k for k in range(0,10) if k is not i and k not in con]))
+        connects.append(con)
+    net = Network()
+    for i in range(10):
+        net.poses.append(poses[i])
+        net.neuronEnts.append(Network.NeuronEnt((poses[i],connects[i],1)))
+    net.initConnects()
+        
+    
+    #neus.append(NeuronEnt((poses,i,connects[i],1)))
+    return net
+
+def initCosmetics():
     fov = 40
     camera.orthographic =  True
     EditorCamera()
-    app = Ursina()
+    
     PointLight(parent=Entity(),x=5,y=5,z=-5,shadows=True)
-    #PointLight(parent=Entity(),x=-1000,y=-1000,z=-500,shadows=True)
+    #AmbientLight(shadows=True,color=(1,1,1,0.1))#PointLight(parent=Entity(),x=-1000,y=-1000,z=-500,shadows=True)
+
+def startUp():
+    network = initNetwork()
+    app = Ursina()
+    initCosmetics()
     app.run()
 
 if __name__ == "__main__":
